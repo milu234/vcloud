af9cce0410babe182ffd156d4fc75a0d97a1aaf9
Rajpreet16 <rajpreet24033@gmail.com>
2018-09-25 09:35:26 +0530

    Routes and Security DOne

--- app/Http/Controllers/HodController.php
+++ app/Http/Controllers/HodController.php
@@ -6,19 +6,32 @@ use App\Exports\UsersExport;
 use Illuminate\Http\Request;
 use App\User;
 use DB;
+use Auth;
 
 class HodController extends Controller
 {
-    public function see(){
-        $users = DB::table('users')
-                ->join('staff__components','staff__components.id','=','users.id')
-                ->select('users.name','users.email','staff__components.item_name','staff__components.working','staff__components.spare')
-                ->get();
-                return view('hodrequest')->with('users',$users);
+    
+        public function see(){
+            if(auth()->check() && auth()->user()->is_hod()) {
+            $users = DB::table('users')
+                    ->join('staff__components','staff__components.id','=','users.id')
+                    ->select('users.name','users.email','staff__components.item_name','staff__components.working','staff__components.spare')
+                    ->get();
+                    return view('hodrequest')->with('users',$users);
+        }
+        else{
+            return redirect()->route('wel');
+        }
     }
+    
 
     public function export($type){
-        return Excel::download(new UsersExport, 'staff__components.' . $type);
+        if(auth()->check() && auth()->user()->is_hod()) {
+            return Excel::download(new UsersExport, 'staff__components.' . $type);
+        }
+        else{
+            return redirect()->route('wel');
+        }
     }
 
 }
--- resources/views/princi.blade.php
+++ resources/views/princi.blade.php
@@ -80,3 +80,55 @@
 </div>
     
 @endsection
+
+
+@section('dashboard_name')
+Welcome {{$data['name']}}
+    @stop
+@section('personal')
+<div class="row ">
+        <div class="col-md-4 images-wala">
+          <img src="{{ URL::to('/img/user.png') }}" height=200px width=200px alt="person" class="img"/>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">{{ __('Personal Info') }}</div>
+
+                <div class="card-body">
+                    <form method="POST">
+                      
+                        <div class="form-group row">
+                            <label for="email" class="col-sm-4 col-form-label">{{ __('Full Name :') }}</label>
+                            
+                            {{ Auth::user()->name }} 
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label">{{ __('Role :') }}</label>
+
+                            {{ $data['role'] }} 
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label">{{ __('Branch :') }}</label>
+
+                            {{ $data['branch'] }}     
+                        </div>
+
+                        
+                        
+                    </form>
+                
+            </div>
+          </div>
+        </div>
+@stop
+@section('card')
+</div>
+<div class="box1 pull-left"><h2><span class="counter">{{$data['total']}}</span>+</h2>Total Request</div>
+<div class="box2 pull-left"><h2><span class="counter">{{$data['current']}}</span>+</h2>Current Request</div>
+<div class="box3 pull-left"><h2><span class="counter">{{$data['pending']}}</span>+</h2>Pending Request</div>
+<div class="box3 pull-left"><h2><span class="counter">{{$data['received']}}</span>+</h2>Received Request</div>
+</div>
+@stop
+
